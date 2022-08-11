4e3e6c8f2f7db5bedd3d7be979e6a72724d7da79
BitIntroverted <lukefriedrich95@gmail.com>
2022-08-11 02:33:45 -0400

    update email

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Roanoke Bakery Coming Soon...</h1>
         <h3>Custom Cake Orders</h3>
-        <h6>Contact: roanokebakeryva@gmail.com</h6>
+        <h6>Contact: info@roanokebakery.com</h6>
         <div>{Gallery()}</div>
         <footer>Est 2022</footer>
       </header>
