50b3d256d823aa7420c80ccbdb2f203419d9bbbe
BitIntroverted <lukefriedrich95@gmail.com>
2022-08-11 03:25:52 -0400

    updating metadata and tab

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         <h1>Roanoke Bakery Coming Soon...</h1>
         <h3><em>Custom Cake & Cookie Orders</em></h3>
         <div>{Gallery()}</div>
-        <footer> <small>Est 2022</small>
-          <div>Contact: info@roanokebakery.com</div>
+        <footer>
           <a target="_blank" href="https://www.instagram.com/shannonknight3074/">
-          <img className="sm_icon" src={require("./photos/insta.png")}/>
-</a>
+          <img className="sm_icon" src={require("./photos/insta.png")}/></a>
+          <div><small>Est 2022</small></div>
+          <div>Contact: info@roanokebakery.com</div>
         </footer>
       </header>
     </div>
